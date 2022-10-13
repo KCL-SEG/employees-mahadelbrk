c857854df3cd58e69da7605387466f931e024f64
root <root@DESKTOP-99L9KP9.localdomain>
2022-10-13 13:27:09 +0100

    employee exercise 4

--- employee.py
+++ employee.py
@@ -1,12 +1,6 @@
-# can change the way the billie, charlie, renee, jan, robbie, and ariel objects are constructed
-# can construct additional objects or objects or different types
-# the total pay MUST be calculated from the parameters provided using the formulae explained
-# can NOT add arguments to the get_pay and the _str_ methods
-
-
 
 class Employee:
-    def __init__(self, name, salary , wage, hours, commission, commissionVal, bonus):
+    def __init__(self, name, salary , wage, hours, commission, commissionVal, bonus): #Constructor is long
         self.name = name
         self.salary = salary
         self.wage = wage
@@ -15,6 +9,7 @@ class Employee:
         self.commissionVal = commissionVal
         self.bonus = bonus
 
+    
     def commissionType(self):
         if (self.commission > 0):
             return f' and receives a commission for {self.commission} contract(s) at {self.commissionVal}/contract'
@@ -31,12 +26,12 @@ class Employee:
         else:
             return ''
 
-# depends the type of contract they have and what they have done in the past month.
+
     def get_pay(self):
         return self.salary + (self.wage * self.hours) + self.bonus + (self.commission * self.commissionVal)
     
-# produce a string explaining how the pay has been calculated.
-    def _str_(self):
+
+    def __str__(self):
         contractType = self.contractType()
         commissionType = self.commissionType()
         totalPay = self.get_pay()
@@ -44,7 +39,7 @@ class Employee:
 
 
 
-# Billie works on a monthly salary of 4000.  Their total pay is 4000.
+
 billie = Employee('Billie', 4000, 0, 0, 0, 0, 0)
 
 
@@ -62,3 +57,12 @@ charlie = Employee('Charlie', 0, 100, 25, 0, 0, 0)
 
 # Ariel works on a contract of 120 hours at 30/hour and receives a bonus commission of 600.  Their total pay is 4200.
 ariel = Employee('Ariel', 0, 120, 30, 0, 0, 600)
+
+print(billie.__str__())
+print(renee.__str__())
+print(robbie.__str__())
+print(jan.__str__())
+print(charlie.__str__())
+print(ariel.__str__())
+
+
